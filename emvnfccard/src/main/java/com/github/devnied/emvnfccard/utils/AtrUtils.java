444ac8688a0b317cd4c61e817d69921b01f4e3b8
CkEsc <iamckesc@gmail.com>
2017-03-21 03:59:52 +0300

    Inline org.greenrobot:essentials dependency.
    - Include Multimap into project

--- emvnfccard/src/main/java/com/github/devnied/emvnfccard/utils/AtrUtils.java
+++ emvnfccard/src/main/java/com/github/devnied/emvnfccard/utils/AtrUtils.java
@@ -22,7 +22,6 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.Collection;
 
-import org.greenrobot.essentials.collections.Multimap;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
