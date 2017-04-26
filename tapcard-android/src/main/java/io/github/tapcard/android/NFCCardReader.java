641cf52928d12fe89995e3aff7f6bb857b2ac8a8
CkEsc <iamckesc@gmail.com>
2017-04-27 00:49:27 +0300

    Reformat code

--- tapcard-android/src/main/java/io/github/tapcard/android/NFCCardReader.java
+++ tapcard-android/src/main/java/io/github/tapcard/android/NFCCardReader.java
@@ -5,109 +5,111 @@ import android.content.Intent;
 import android.nfc.NfcAdapter;
 import android.nfc.Tag;
 import android.nfc.tech.IsoDep;
+
+import java.io.IOException;
+
 import io.github.tapcard.emvnfccard.log.Logger;
 import io.github.tapcard.emvnfccard.log.LoggerFactory;
 import io.github.tapcard.emvnfccard.model.EmvCard;
 import io.github.tapcard.emvnfccard.parser.EmvParser;
-import java.io.IOException;
 
 public class NFCCardReader {
-  private NFCUtils nfcUtils;
-  private AndroidNfcProvider provider;
-  private Logger logger;
+    private NFCUtils nfcUtils;
+    private AndroidNfcProvider provider;
+    private Logger logger;
 
-  public NFCCardReader(Activity activity) {
-    nfcUtils = new NFCUtils(activity);
-    provider = new AndroidNfcProvider();
-    logger = LoggerFactory.getLogger(NFCCardReader.class);
-  }
-
-  /**
-   * Begin waiting for bank card been tapped to NFC module of phone
-   */
-  public void enableDispatch() {
-    nfcUtils.enableDispatch();
-  }
-
-  /**
-   * Stop waiting for bank card
-   */
-  public void disableDispatch() {
-    nfcUtils.disableDispatch();
-  }
-
-  /**
-   * Checks that intent is suitable for NFC card reading
-   *
-   * @param intent intent for check
-   * @return true - intent is good. false - this is not the intent you're looking for
-   */
-  public boolean isSuitableIntent(Intent intent) {
-    final Tag tag = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
-    if (tag == null) {
-      logger.debug("No TAG in intent");
-      return false;
+    public NFCCardReader(Activity activity) {
+        nfcUtils = new NFCUtils(activity);
+        provider = new AndroidNfcProvider();
+        logger = LoggerFactory.getLogger(NFCCardReader.class);
     }
 
-    IsoDep tagComm = IsoDep.get(tag);
-    if (tagComm == null) {
-      logger.debug("IsoDep was not enumerated in getTechList()");
-      return false;
+    /**
+     * Begin waiting for bank card been tapped to NFC module of phone
+     */
+    public void enableDispatch() {
+        nfcUtils.enableDispatch();
     }
 
-    return true;
-  }
-
-  /**
-   * Read card data from given intent.
-   * <p>Note that this method is blocking. You should not use it as is.
-   * Instead - wrap it into some async framework: RxJava, AsyncTask, etc</p>
-   *
-   * <p>Intent by itself does not contain all data. It contain metadata of NFC card.
-   * For reading data library will open NFC connection and read byte data from card</p>
-   *
-   * <p>You should check that this intent contain right data with {@link #isSuitableIntent(Intent)}
-   * before calling this method</p>
-   *
-   * @param intent intent with initial card information.
-   * @return Ready for use card data
-   * @throws IOException may be thrown during NFC data transfer
-   * @throws WrongIntentException thrown if intent does not contain {@link NfcAdapter#EXTRA_TAG}
-   * @throws WrongTagTech thrown when this NFC tech is not supported:
-   * not enumerated in {@link Tag#getTechList}.
-   */
-  public EmvCard readCardBlocking(Intent intent)
-      throws IOException, WrongIntentException, WrongTagTech {
-    final Tag tag = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
-    if (tag == null) {
-      throw new WrongIntentException("No TAG in intent");
+    /**
+     * Stop waiting for bank card
+     */
+    public void disableDispatch() {
+        nfcUtils.disableDispatch();
     }
 
-    IsoDep tagComm = IsoDep.get(tag);
-    if (tagComm == null) {
-      throw new WrongTagTech();
+    /**
+     * Checks that intent is suitable for NFC card reading
+     *
+     * @param intent intent for check
+     * @return true - intent is good. false - this is not the intent you're looking for
+     */
+    public boolean isSuitableIntent(Intent intent) {
+        final Tag tag = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
+        if (tag == null) {
+            logger.debug("No TAG in intent");
+            return false;
+        }
+
+        IsoDep tagComm = IsoDep.get(tag);
+        if (tagComm == null) {
+            logger.debug("IsoDep was not enumerated in getTechList()");
+            return false;
+        }
+
+        return true;
     }
 
-    try {
-      tagComm.connect();
-      provider.setmTagCom(tagComm);
+    /**
+     * Read card data from given intent.
+     * <p>Note that this method is blocking. You should not use it as is.
+     * Instead - wrap it into some async framework: RxJava, AsyncTask, etc</p>
+     * <p>
+     * <p>Intent by itself does not contain all data. It contain metadata of NFC card.
+     * For reading data library will open NFC connection and read byte data from card</p>
+     * <p>
+     * <p>You should check that this intent contain right data with {@link #isSuitableIntent(Intent)}
+     * before calling this method</p>
+     *
+     * @param intent intent with initial card information.
+     * @return Ready for use card data
+     * @throws IOException          may be thrown during NFC data transfer
+     * @throws WrongIntentException thrown if intent does not contain {@link NfcAdapter#EXTRA_TAG}
+     * @throws WrongTagTech         thrown when this NFC tech is not supported:
+     *                              not enumerated in {@link Tag#getTechList}.
+     */
+    public EmvCard readCardBlocking(Intent intent)
+            throws IOException, WrongIntentException, WrongTagTech {
+        final Tag tag = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
+        if (tag == null) {
+            throw new WrongIntentException("No TAG in intent");
+        }
 
-      EmvParser parser = new EmvParser(provider, true);
-      return parser.readEmvCard();
-    } finally {
-      tagComm.close();
-    }
-  }
+        IsoDep tagComm = IsoDep.get(tag);
+        if (tagComm == null) {
+            throw new WrongTagTech();
+        }
 
-  public static class WrongIntentException extends Exception {
-    WrongIntentException(String detailMessage) {
-      super(detailMessage);
-    }
-  }
+        try {
+            tagComm.connect();
+            provider.setmTagCom(tagComm);
 
-  public static class WrongTagTech extends Exception {
-    WrongTagTech() {
-      super("IsoDep was not enumerated in getTechList()");
+            EmvParser parser = new EmvParser(provider, true);
+            return parser.readEmvCard();
+        } finally {
+            tagComm.close();
+        }
     }
-  }
+
+    public static class WrongIntentException extends Exception {
+        WrongIntentException(String detailMessage) {
+            super(detailMessage);
+        }
+    }
+
+    public static class WrongTagTech extends Exception {
+        WrongTagTech() {
+            super("IsoDep was not enumerated in getTechList()");
+        }
+    }
 }
